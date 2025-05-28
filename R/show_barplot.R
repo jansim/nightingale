83d89572213f6a94a09d4e45c62b5bc150d6ae04
Jan Simson <git@simson.io>
2025-05-28 09:29:05 +0200

    Prefix package imports

--- R/show_barplot.R
+++ R/show_barplot.R
@@ -1,26 +1,26 @@
 show_barplot <- function(mortality_data = mortality, include_total = FALSE) {
   if (!include_total) {
     mortality_data <- mortality_data |>
-      select(-total)
+      dplyr::select(-total)
   }
 
   # Inspired by:
   # https://www.datawrapper.de/blog/recreating-nightingale-rose-chart
   mortality_data |>
-    pivot_longer(-date) |>
-    ggplot() +
-    aes(
+    tidyr::pivot_longer(-date) |>
+    ggplot2::ggplot() +
+    ggplot2::aes(
       x = date,
       y = value,
       fill = name
     ) +
-    geom_col(position = "dodge") +
-    theme_classic() +
-    labs(
+    ggplot2::geom_col(position = "dodge") +
+    ggplot2::theme_classic() +
+    ggplot2::labs(
       x = "",
       y = "Deaths per 1000 soldiers per year"
     ) +
-    scale_x_datetime(date_labels = "%B %Y") +
+    ggplot2::scale_x_datetime(date_labels = "%B %Y") +
     # Nightingale's improvements were implemented in Feb / March 1855
-    geom_vline(xintercept = as.POSIXct("1855-02-15"), linetype = "dashed")
+    ggplot2::geom_vline(xintercept = as.POSIXct("1855-02-15"), linetype = "dashed")
 }
