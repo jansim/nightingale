da338fb9d5fd33d130cfdbe5976e5e3af5322f40
Jan Simson <git@simson.io>
2025-09-21 19:39:07 +0200

    Remove snapshot test

--- tests/testthat/test-show_barplot.R
+++ tests/testthat/test-show_barplot.R
@@ -32,16 +32,3 @@ test_that("show_barplot should validate metrics", {
     show_barplot(test_mortality_data, metrics = c("disease", "invalid_metric"))
   )
 })
-
-test_that("show_barplot visual appearance", {
-  plot <- show_barplot(test_mortality_data, highlight_intervention = FALSE)
-  suppressMessages({
-    plot <- plot +
-      ggplot2::scale_x_date(
-        date_breaks = ggplot2::waiver(),
-        breaks = as.Date(c("1854-04-01", "1854-05-01", "1854-06-01")),
-        labels = scales::label_date(format = "%m/%Y", locale = NULL)
-      )
-  })
-  vdiffr::expect_doppelganger("barplot_default", plot)
-})
